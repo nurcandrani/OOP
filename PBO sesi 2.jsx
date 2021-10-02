ba7f59575ef469a54762fee8342a5061e6abb96d
Nur Candrani <nurcandrani311@gmail.com>
2021-10-03 00:19:37 +0700

    Add files via upload

---
+++ PBO sesi 2.jsx
@@ -0,0 +1,12 @@
+public class BasicJava{
+    public static void main (string[] args)
+    {
+        int tahunLahir = 1998;
+        int tahunSekarang = 2020;
+        int usia = tahunSekarang - tahunLahir;
+        System.out.print("Nur Candrani lahir di tahun " + tahunLahir + ". ");
+        System.out.println("Sekarang sudah tahun " + tahunSekarang);
+        System.out.println("Berarti, usia Nur Candrani sekarang adalah " + usia + " tahun " );
+    }
+}
+ 
